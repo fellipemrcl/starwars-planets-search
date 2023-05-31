bfc2685edfe37badef3b4ba86529a9f1a7a09e45
Fellipe Marcel <fmrclms@gmail.com>
2023-05-31 15:45:52 -0300

    feat: implemented text filter

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,42 +1,52 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import PlanetsContext from '../context/Context';
 
 function Table() {
   const { planets } = useContext(PlanetsContext);
+  const [inputSearch, setInputSearch] = useState('');
 
   return (
-    <table>
-      <thead>
-        {planets && planets.length > 0 && (
-          <tr>
-            {Object.keys(planets[0]).map((key, i) => (
-              <th key={ i } scope="col">
-                {key}
-              </th>
+    <div>
+      <input
+        type="text"
+        data-testid="name-filter"
+        onChange={ ({ target }) => setInputSearch(target.value) }
+      />
+      <table>
+        <thead>
+          {planets && planets.length > 0 && (
+            <tr>
+              {Object.keys(planets[0]).map((key, i) => (
+                <th key={ i } scope="col">
+                  {key}
+                </th>
+              ))}
+            </tr>
+          )}
+        </thead>
+        <tbody>
+          { planets
+            .filter((planet) => planet.name.toLowerCase().includes(inputSearch))
+            .map((planet, i) => (
+              <tr key={ i }>
+                <td>{planet.name}</td>
+                <td>{planet.rotation_period}</td>
+                <td>{planet.orbital_period}</td>
+                <td>{planet.diameter}</td>
+                <td>{planet.climate}</td>
+                <td>{planet.gravity}</td>
+                <td>{planet.terrain}</td>
+                <td>{planet.surface_water}</td>
+                <td>{planet.population}</td>
+                <td>{planet.films}</td>
+                <td>{planet.created}</td>
+                <td>{planet.edited}</td>
+                <td>{planet.url}</td>
+              </tr>
             ))}
-          </tr>
-        )}
-      </thead>
-      <tbody>
-        { planets.map((planet, i) => (
-          <tr key={ i }>
-            <td>{planet.name}</td>
-            <td>{planet.rotation_period}</td>
-            <td>{planet.orbital_period}</td>
-            <td>{planet.diameter}</td>
-            <td>{planet.climate}</td>
-            <td>{planet.gravity}</td>
-            <td>{planet.terrain}</td>
-            <td>{planet.surface_water}</td>
-            <td>{planet.population}</td>
-            <td>{planet.films}</td>
-            <td>{planet.created}</td>
-            <td>{planet.edited}</td>
-            <td>{planet.url}</td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
+        </tbody>
+      </table>
+    </div>
   );
 }
 
